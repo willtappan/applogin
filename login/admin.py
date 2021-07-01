f4b7bc3f290b665440154a71ba02c91b99495ec8
William <ing.tappan@gmail.com>
2021-06-30 22:58:48 -0500

    Registros en el home

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+
+
+# Models
+from django.contrib.auth.models import User
+from login.models import Registro
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+
+    model = Registro
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
